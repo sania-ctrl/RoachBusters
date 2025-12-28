ee4d82f4aaf6ced1fe2e2d1fa3137733abdc21c0
Sania S <136681501+sania-ctrl@users.noreply.github.com>
2025-12-28 00:35:35 -0500

    Refactor voicemodule.py for clarity and updates
    Updated API key placeholder and removed installation instructions.

--- voicemodule.py
+++ voicemodule.py
@@ -1,48 +1,30 @@
-# export API key
-# Linux/MacOs
-# export ELEVENLABS_API_KEY="Insert_API_KEY"
-# Windows
-# set ELEVENLABS_API_KEY="Insert_API_KEY"
-
-# INSTALL PYTHON PLAYSOUND BEFORE USING WITH THE FOLLOWING COMMAND
-# pip install playsound==1.2.2
-
-# ALSO INSTALL ELEVENLABS PACKAGE WITH THE FOLLOWING COMMANDS
-# pip install elevenlabs
-# npm install @elevenlabs/elevenlabs-js
-
-# install ffmpeg 
-# 
-
-
-from elevenlabs import ElevenLabs #set_api_key, generate 
 from elevenlabs.play import play
 from pathlib import Path
 import os
 from playsound import playsound
-os.environ["ELEVENLABS_API_KEY"] = "Insert_API_KEY"
-
+os.environ["ELEVENLABS_API_KEY"] = "insert_API_key"
 
 
 def getVoice(filename):
-
     elevenlabs = ElevenLabs(
-
         api_key=os.getenv("ELEVENLABS_API_KEY"),
     )
     file_content = Path(filename).read_text()
-  
+    # with open("test.txt", "r", encoding="utf-8") as f:
+    #     text = f.read()
     audio = elevenlabs.text_to_speech.convert(
         voice_id="DLsHlh26Ugcm6ELvS0qi",
         output_format="mp3_44100_128",
         model_id="eleven_multilingual_v2",
         text=file_content
     )
+
+    play(audio)
     with open(f"/home/rozilla/speech.mp3", "wb") as f:
         for chunk in audio:
             if chunk:
                 f.write(chunk)
-    
+
     os.system(f"mpv /home/rozilla/speech.mp3")
 
 #getVoice("test.txt")
